017348a4890acd43a6b2a7f98433a266343fe828
Joshua Monday <bumiche@gmail.com>
2023-12-16 04:53:52 -0800

    ok

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('register-customer/', views.register, name= 'register-customer'),
     path('login/', views.login_user, name= 'login'),
     path('logout/', views.logout_user, name='logout'),
