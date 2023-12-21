772f86e53699d528093ed7705e2a250992f9eb00
Joshua Monday <bumiche@gmail.com>
2023-12-20 20:03:46 -0800

    useradmin created

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets= (
+        UserAdmin.fieldsets(
+            'Custom Field Heading',
+            {
+                'fields'(
+                    'is_cutomer',
+                    'is_engineer'
+                )
+            }
+        )
+    )
