49d59ff358cec8ee4623b148e9e006d16e09e2e8
Joshua Monday <bumiche@gmail.com>
2024-01-02 08:15:51 -0800

    done

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from django.contrib import admin
 from . import views
-from users.views import login
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register-customer/', views.register, name= 'register-customer'),
-    path('login/', views.login_user, name= 'login'),
+    # path('login/', views.login_user, name= 'login'),
     path('logout/', views.logout_user, name='logout'),
     path('buy_ticket/', views.buy_ticket, name= 'buy_ticket')
-    path('api/login', login, name='login')
+    path('api/login/', views.login, name='login'),
 
 ]
